d342065eefe8d94a199da845795af5990154552c
atharva030 <atharvas_jagdale@auton.in>
2023-01-25 18:34:31 +0530

    text change

--- rmageddontest01-main/src/Components/Hero.jsx
+++ rmageddontest01-main/src/Components/Hero.jsx
@@ -12,16 +12,20 @@ const Hero = () => {
         className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
       >
         <div className="flex flex-row justify-between items-center w-full">
-        <h1 className="flex-1 font-poppins font-semibold ss:text-[76px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-          <span className="text-gradient">RMAGEDDON 2023 </span>{" "}<br></br>          
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[76px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            <span className="text-gradient">RMAGEDDON 2023 </span> <br></br>
           </h1>
         </div>
 
         <h1 className="font-poppins  font-semibold ss:text-[76px] text-[52px]  text-white ss:leading-[80.8px] leading-[75px] w-full">
-        Experience the <br/>future of robotics
+          Experience the <br />
+          future of robotics
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Get ready to unleash the potential of your robotic creations and also get ready to join the elite in the field of robotics at RNXG and compete for recognition and prizes at the Rmageddon
+          Unleash the full potential of your robotic creations and showcase your
+          skills to the elite in the field of robotics at RNXG. Compete for
+          recognition and prizes at Rmageddon and be a part of the most exciting
+          college robotics event of the year!
         </p>
       </div>
       <div
@@ -29,7 +33,7 @@ const Hero = () => {
       >
         <img
           src={rmageddon}
-          alt="rmageddon" 
+          alt="rmageddon"
           className="object-contain scale-[1.2] w-[80%] h-[80%]  relative z-[5] head-image"
         />
         <div className="absolute z-[0] w-[40%] h-[20%] top-0 g__gradient" />
